48a62c32326e8b2bd25d151aeed314cd03d4a9eb
AutomotiveNegotiation1 <sh0824.jang@gmail.com>
2021-12-28 17:13:59 +0900

    Initial commit

---
+++ turtlebot3_tunnel_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+gnome-terminal -e "roslaunch turtlebot3_gazebo turtlebot3_tunnel.launch"
+
+sleep 2
+
+gnome-terminal -e "roslaunch pozyx_simulation uwb_anchors_set_tunnel.launch" 
+
+sleep 2
+
+gnome-terminal -e "rosrun pozyx_simulation uwb_simulation.py"
+
+sleep 2
+
+export TURTLEBOT3_MODEL=waffle_pi
+
+gnome-terminal -e "roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch"
+
